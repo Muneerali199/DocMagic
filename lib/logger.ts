4d4af39f4e1f7db920973f914903d36c45f5d856
neethihenavalaparla03 <neethihenavalaparla03@gmail.com>
2026-05-15 17:00:23 +0530

    Replaced console logs with logger utility

---
+++ lib/logger.ts
@@ -0,0 +1,23 @@
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+export const logger = {
+  debug: (...args: unknown[]) => {
+    if (isDevelopment) {
+      console.log(...args);
+    }
+  },
+
+  info: (...args: unknown[]) => {
+    if (isDevelopment) {
+      console.info(...args);
+    }
+  },
+
+  warn: (...args: unknown[]) => {
+    console.warn(...args);
+  },
+
+  error: (...args: unknown[]) => {
+    console.error(...args);
+  },
+};
