cee59de36fa666c147ce35eb1cba50a8a19d2bdb
Tanishk <tanishksinhasw007@gmail.com>
2026-05-30 07:50:02 +0530

    fix: resolve TS type issue with tracesSampleRate environment variable

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -4,9 +4,7 @@ Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE 
-    ? parseFloat(process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE) 
-    : 0.2,
+  tracesSampleRate: parseFloat(process.env.NEXT_PUBLIC_SENTRY_TRACES_SAMPLE_RATE || "0.2"),
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
